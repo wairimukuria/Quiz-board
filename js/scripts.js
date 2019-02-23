7efd7a86a0a74b3c753f3283862bbacb414d59ab
wairimukuria <mimukuria@gmail.com>
2019-02-23 14:20:28 +0300

    added an variable thats an array of the correct answers and another variable pointPerCorrect with a value of points a correct answer holds

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
-var answers = ["infinity","firstName","vowels"];
+var answers = ["Not a Number","no value or no object","return","ampersand , semicolon","\"];
 var pointPerCorrect = 25;
 
 
 function percentage(score) {
-return "Your score is " + parseInt((score / 75) * 100) + "%";
+return "Your score is " + parseInt((score / 125) * 100) + "%";
 }
 
 $(document).ready(function(){
@@ -15,8 +15,10 @@ $(document).ready(function(){
       var answerOne = ($("input[type=radio][name=questionOneAnswer]:checked").val());
       var answerTwo = ($("input[type=radio][name=questionTwoAnswer]:checked").val());
       var answerThree = ($("input[type=radio][name=questionThreeAnswer]:checked").val());
+      var answerFour = ($("input[type=radio][name=questionFourAnswer]:checked").val());
+      var answerFive = ($("input[type=radio][name=questionFiveAnswer]:checked").val());
 
-      if (answerOne === undefined || answerTwo === undefined || answerThree === undefined) {
+  if (answerOne === undefined || answerTwo === undefined || answerThree === undefined || answerFour === undefined || answerFive === undefined) {
   $('#questionsIncomplete').text('Please Complete questions Before Submitting');
   $('#questionsIncomplete').fadeOut(10000);
 } else {
@@ -29,11 +31,19 @@ $(document).ready(function(){
          if (answerThree === answers[2]) {
     score += pointPerCorrect;
   }
+        if (answerFour === answers[3]) {
+    score += pointPerCorrect;
+}
+        if (answerFive === answers[4]) {
+    score += pointPerCorrect;
+}
 
 
         $("input[type=radio][name=questionOneChoice]:checked").prop('checked', false);
         $("input[type=radio][name=questionTwoChoice]:checked").prop('checked', false);
         $("input[type=radio][name=questionThreeChoice]:checked").prop('checked', false);
+        $("input[type=radio][name=questionFourChoice]:checked").prop('checked', false);
+        $("input[type=radio][name=questionFiveChoice]:checked").prop('checked', false);
         $('#questionsIncomplete').text('');
         $('#result').text(percentage(score));
 }
